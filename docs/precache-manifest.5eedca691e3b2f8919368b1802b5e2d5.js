c97cac8f8a0b7d11f610564c83cc5d7199c78fab
gsmith <gsmith077@users.noreply.github.com>
2020-09-21 18:14:40 -0400

    creating the /docs/ directory to cheat a bit at the ghpages hosting.

---
+++ docs/precache-manifest.5eedca691e3b2f8919368b1802b5e2d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a845ed835b2189605eee45c75a60968b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ddede8b31102c0747b36",
+    "url": "./static/css/main.ee236597.chunk.css"
+  },
+  {
+    "revision": "30c63894a83261225d21",
+    "url": "./static/js/2.a38357e2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.a38357e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddede8b31102c0747b36",
+    "url": "./static/js/main.65f483f0.chunk.js"
+  },
+  {
+    "revision": "4231b9fff86d0de7c95a",
+    "url": "./static/js/runtime-main.feac32b5.js"
+  }
+]);
