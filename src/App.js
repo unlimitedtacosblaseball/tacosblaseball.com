124b79f1ea3d90e1f1f5778465b36db03456537c
gsmith077 <gsmith077@github.com>
2020-10-18 18:04:48 -0400

    Added links to friends. =)

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Index from './pages/index/index';
 import Snackrifice from './pages/snackrifice/snackrifice';
 import seasonATE from './pages/seasonate/seasonate';
 import Resources from './pages/resources/resources';
+import Friends from './pages/friends/friends';
 import Lost from './pages/lost/lost';
 // Add your pages here.
 import Template from './pages/template/template'
@@ -16,6 +17,7 @@ function App() {
         <Route path="/snackrifice" component={ Snackrifice } />
         <Route path="/seasonate" component={ seasonATE } />
         <Route path="/resources" component={ Resources } />
+        <Route path="/friends" component={ Friends } />
         <Route path="/template" component={ Template } />
         <Route path="/" component={ Lost } />
       </Switch>
--- src/pages/index/index.md
+++ src/pages/index/index.md
@@ -6,6 +6,8 @@
 
 ### <span class="nav_link yellow">[View team resources](/resources)</span>
 
+### <span class="nav_link yellow">[Meet our friends](/friends)</span>
+
 <div class="img-box">
 
 ![This taco is under construction, check back soon.](https://media.discordapp.net/attachments/755560065645281360/755577280989888562/tacoconstruction.png)
