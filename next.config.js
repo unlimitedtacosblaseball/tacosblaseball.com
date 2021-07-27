8ae4e048bb957235d652e9c59afb94ea1fa59b1f
gsmith077 <33425559+gsmith077@users.noreply.github.com>
2021-07-26 23:25:37 -0400

    Working stuff that deploys

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
+const { default: next } = require("next");
+
 const withMDX = require("@next/mdx")({
-  extension: /\.mdx?$/
+  extension: /\.mdx?$/,
 });
 
 module.exports = withMDX({
+  trailingSlash: true,
   pageExtensions: ["js", "jsx", "md", "mdx"]
-});
+})
